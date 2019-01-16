af5e86dde63b4259a5c0888f1bcd7107d1290fff
Mustafa Senol Cosar <mustafa@unscrambl.com>
2019-01-16 16:07:01 +0300

    add unsigned test case

--- test/jsonLong.spec.js
+++ test/jsonLong.spec.js
@@ -8,6 +8,19 @@ describe("Tests the Long support", function(){
     var input = '{"longMaxValue":9223372036854775807,"longMinValue":-9223372036854775807,"number":123}';
 
     it("tests the JSON parse and stringify functionality", function(done){
+        var parsedObject = JSONLong.parse(input);
+        expect(parsedObject.number.toString()).to.equal("123");
+        expect(parsedObject.longMaxValue.toString()).to.equal("9223372036854775807");
+        expect(parsedObject.longMaxValue).to.be.instanceof(Long);
+        expect(parsedObject.longMinValue.toString()).to.equal("-9223372036854775807");
+        expect(parsedObject.longMinValue).to.be.instanceof(Long);
+
+        var expectedObject = JSONLong.stringify(parsedObject);
+        expect(expectedObject).to.equal(input);
+        done();
+    });
+
+    it("tests the JSON parse and stringify functionality with unsigned support", function(done){
         var parsedObject = JSONLong.parse(input, {unsigned: true});
         expect(parsedObject.number.toString()).to.equal("123");
         expect(parsedObject.longMaxValue.toString()).to.equal("9223372036854775807");
