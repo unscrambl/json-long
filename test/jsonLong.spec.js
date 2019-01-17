7ee9a35b2ea58d612988dac8984135133762c822
Mustafa Senol Cosar <mustafa@unscrambl.com>
2019-01-17 17:36:13 +0300

    Improve the tests for unsigned long values

--- test/jsonLong.spec.js
+++ test/jsonLong.spec.js
@@ -1,5 +1,3 @@
-var mocha  = require('mocha');
-var assert = require('chai').assert;
 var expect = require('chai').expect;
 var Long = require('long');
 var JSONLong = require('../index');
@@ -9,19 +7,6 @@ describe("Tests the Long support", function(){
 
     it("tests the JSON parse and stringify functionality", function(done){
         var parsedObject = JSONLong.parse(input);
-        expect(parsedObject.number.toString()).to.equal("123");
-        expect(parsedObject.longMaxValue.toString()).to.equal("9223372036854775807");
-        expect(parsedObject.longMaxValue).to.be.instanceof(Long);
-        expect(parsedObject.longMinValue.toString()).to.equal("-9223372036854775807");
-        expect(parsedObject.longMinValue).to.be.instanceof(Long);
-
-        var expectedObject = JSONLong.stringify(parsedObject);
-        expect(expectedObject).to.equal(input);
-        done();
-    });
-
-    it("tests the JSON parse and stringify functionality with unsigned support", function(done){
-        var parsedObject = JSONLong.parse(input, {unsigned: true});
         expect(parsedObject.number.toString()).to.equal("123");
         expect(parsedObject.longMaxValue.toString()).to.equal("9223372036854775807");
         expect(parsedObject.longMaxValue).to.be.instanceof(Long);
